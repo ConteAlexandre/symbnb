0f097c181417b003ba7c0c6d33ec8852d16793e3
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-09-26 23:15:00 +0200

    Personalization Form with the css and the attributes

--- src/Form/AdCreateType.php
+++ src/Form/AdCreateType.php
@@ -10,6 +10,11 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,15 +26,31 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class AdCreateType extends AbstractType
 {
+    /**
+     * @param $label
+     * @param $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder)
+    {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('introduction')
-            ->add('content')
-            ->add('rooms')
-            ->add('price')
-            ->add('coverImage')
+            ->add('title', TextType::class, $this->getConfiguration('Title', 'Title of announcement'))
+            ->add('slug', TextType::class, $this->getConfiguration('Slug', 'Web Address (auto)'))
+            ->add('coverImage', UrlType::class, $this->getConfiguration('Url of the image', 'Give the address for the beautiful image'))
+            ->add('introduction', TextType::class, $this->getConfiguration('Introduction', 'Write description for this announcement'))
+            ->add('content', TextareaType::class, $this->getConfiguration('Content', 'Write the all description'))
+            ->add('rooms', IntegerType::class, $this->getConfiguration('Number Rooms', 'Enter the number rooms for the announcement'))
+            ->add('price', MoneyType::class, $this->getConfiguration('Price for one night', 'Write the price'))
         ;
     }
 
