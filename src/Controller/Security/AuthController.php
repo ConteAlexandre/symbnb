4c5eebc52131285c78ed84ac143d7c18135ceb4a
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-09-27 11:51:40 +0200

    Created the page profile

--- src/Controller/Security/AuthController.php
+++ src/Controller/Security/AuthController.php
@@ -8,7 +8,11 @@
 
 namespace App\Controller\Security;
 
+use App\Entity\PasswordUpdate;
+use App\Form\UpdatePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -47,5 +51,43 @@ class AuthController extends AbstractController
     public function logout()
     {
 
+    }
+
+    /**
+     * @Route("/update-password", name="update_password")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function updatePassword(Request $request)
+    {
+        $passwordUpdate = new PasswordUpdate();
+        $user = $this->getUser();
+        $form = $this->createForm(UpdatePasswordType::class, $passwordUpdate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Verification the oldPassword between the new password
+            if (!password_verify($passwordUpdate->getOldPassword(), $user->getPassword())) {
+                $form->get('oldPassword')->addError(new FormError("The old password is not equal"));
+            } else {
+                $newPassword = $passwordUpdate->getNewPassword();
+                $password = $this->encoderPassword->encodePassword($user, $newPassword);
+                $user->setPassword($password);
+                $this->em->persist($user);
+                $this->em->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Update password it\'s ok '
+                );
+                return $this->redirectToRoute('homepage');
+            }
+        }
+
+        return $this->render('users/password.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
